6ac22b066d1bf403bf35c3f1c9774c76f827f64a
watchtheblur <watchtheblur@gmail.com>
2014-08-23 16:43:45 -0700

    Add Asset Scripts

---
+++ Assets/Scripts/BulletAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletAI : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = transform.position;
+		newPosition.y += speed * Time.deltaTime;
+		transform.position = newPosition;
+
+	}
+}
