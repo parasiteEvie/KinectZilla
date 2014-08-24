5b62a1c01fc6d5df6d5fba633094fb368c87b4db
Yori Kvitchko <yori@comcast.net>
2014-08-23 21:18:43 -0700

    Tweaking hand movement

--- Assets/Scripts/Zilla/Hand.cs
+++ Assets/Scripts/Zilla/Hand.cs
@@ -7,11 +7,14 @@ public class Hand:MonoBehaviour {
 	public bool leftHand;
 	public Transform followTarget;
 
+	const float RECOVER_Y_POS = 5f;
+
 	private Vector3 startScale;
 
 	private Vector3 startPos;
 	private Vector3 deltaPos;
 	private Vector3 targetPos;
+	float targetZ;
 
 	private bool attacking = false;
 	private bool recovered = true;
@@ -20,20 +23,32 @@ public class Hand:MonoBehaviour {
 	public void Awake() {
 		startPos = transform.position;
 		startScale = transform.localScale;
+		targetZ = startPos.z;
 	}
 
 	// Input
 	public void Update() {
 		// Attack
-		if(transform.position.y < 0f && ! attacking && recovered) {
+		if(transform.position.y < RECOVER_Y_POS && ! attacking && recovered) {
 			HandAttack();
-		} else if(! recovered && transform.position.y > 0f && ! attacking) {
+		} else if(! recovered && transform.position.y > RECOVER_Y_POS && ! attacking) {
 			recovered = true;
+			collider.enabled = true;
 		}
 
 		// Follow
 		if(! attacking) {
-			deltaPos = Vector3.ClampMagnitude(followTarget.position - transform.position, 5f);
+			// Move towards follow target
+			deltaPos = Vector3.ClampMagnitude(followTarget.position - transform.position, 10f);
+			// Z adjustment
+			if(! recovered && transform.position.z < 30f) {
+				deltaPos.z = Time.deltaTime * 300f;
+			} else if(recovered && transform.position.z > 20f) { 
+				deltaPos.z = -Time.deltaTime * 300f;
+			} else {
+				deltaPos.z = 0f;
+			}
+			// Adjust!
 			if(deltaPos.magnitude > 1f) {
 				transform.position += deltaPos * Time.deltaTime * 5f;
 			}
@@ -51,6 +66,6 @@ public class Hand:MonoBehaviour {
 
 	private void DoneHandAttack() {
 		attacking = false;
-		HOTween.To(transform, 0.5f, new TweenParms().Prop("localScale", startScale).Ease(EaseType.EaseInOutCubic));
+		collider.enabled = false;
 	}
 }
