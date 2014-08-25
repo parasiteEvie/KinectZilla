c747da7753888f64622a16018bf0f3fa27925830
Evie Powell <evie@vergeofbrilliance.com>
2014-08-25 12:58:10 -0700

    More of Havis art added to the project.  Also implemented collectible manager.  collectibles are not yet collectable =P

---
+++ Assets/CollectibleGeneratorScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CollectibleType 
+{
+	BOMB,
+	HEALTH_PACK
+}
+
+public class CollectibleGeneratorScript : MonoBehaviour {
+
+	//float genTime;
+	float timer;
+
+	public GameObject healthPack;
+	public GameObject bomb;
+
+	// Use this for initialization
+	void Start () {
+		GetRandomGenTime();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer > 0f){
+			timer -= Time.deltaTime;
+		}
+		else{
+			GetRandomGenTime();
+			//generate a collectible
+			GameObject.Instantiate(healthPack, new Vector3((float)Random.Range (-30f, 30f), 
+			                                               (float)Random.Range (-10.5f, -7.5f),
+			                                               (float)Random.Range (15f, 21f)), Quaternion.identity);
+		}
+	
+	}
+
+	void GetRandomGenTime () {
+		timer = (float)Random.Range (15, 45);
+	}
+}
---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+	float moveAnimation = 1.75f;
+	bool upDirection = true;
+	float speed = .60f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(upDirection)
+		{
+			this.gameObject.transform.position += new Vector3(0f, Time.deltaTime * speed, 0f);
+		}
+		else{
+			this.gameObject.transform.position -= new Vector3(0f, Time.deltaTime * speed, 0f);
+		}
+
+		if (moveAnimation <= 0f){
+			moveAnimation = 1.75f;
+			upDirection = !upDirection;
+		}
+		else{
+			moveAnimation -= Time.deltaTime;
+		}
+
+
+	}
+}
