17f4d9879d6631239d8428fa5bdbb1dc36e3abe3
AbbieDoobie <nathan.p.zager@gmail.com>
2014-08-27 18:22:49 -0700

    Fixed player spawning multiple times
    Occasionally a player could spawn multiple copies of themselves when
    they die. This fix adds a spawn timer for each player. Spawn timer must
    reach 3 seconds to respawn,. Timer resets when player spawns. Timer also
    resets if it goes over 5 seconds.

--- Assets/Prefabs/CreatePlayer.cs
+++ Assets/Prefabs/CreatePlayer.cs
@@ -10,9 +10,23 @@ public class CreatePlayer : MonoBehaviour {
 
 	public GameObject player;
 
+	private double spawntimer1;
+	private double spawntimer2;
+	private double spawntimer3;
+	private double spawntimer4;
+	private double TimeToWait;
+	private bool IsTiming = false;
+
 	private string _isMac;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		spawntimer1 = 3;
+		spawntimer2 = 3;
+		spawntimer3 = 3;
+		spawntimer4 = 3;
+		IsTiming = true;
+		TimeToWait = 3;
 		if ( Application.platform == RuntimePlatform.WindowsEditor ||
 		    Application.platform == RuntimePlatform.WindowsPlayer ||
 		    Application.platform == RuntimePlatform.WindowsWebPlayer) {
@@ -25,26 +39,63 @@ public class CreatePlayer : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(!player1IsActive && Input.GetButtonDown("JumpP1"+_isMac)){; 
+	void Update () 
+	{
+		if (IsTiming == true)
+		{
+			//Add the change in time between frames to the timer
+			spawntimer1 += Time.deltaTime;
+			spawntimer2 += Time.deltaTime;
+			spawntimer3 += Time.deltaTime;
+			spawntimer4 += Time.deltaTime;
+		}
+
+		//reset spawn timer when it gets too high
+		if (spawntimer1 > 5) 
+			{
+				spawntimer1 = 0;
+			}
+		if (spawntimer2 > 5) 
+			{
+				spawntimer2 = 0;
+			}
+		if (spawntimer3 > 5) 
+			{
+				spawntimer3 = 0;
+			}
+		if (spawntimer4 > 5) 
+			{
+				spawntimer4 = 0;
+			}
+		//end reset code
+
+		if(!player1IsActive && Input.GetButtonDown("JumpP1"+_isMac) && spawntimer1 > TimeToWait)
+		{ 
 			player1IsActive = true;
 			PlayerScript p1 = ((GameObject)Instantiate(player, new Vector3(-6f, -3f, 20), Quaternion.identity)).GetComponent<PlayerScript>();
 			p1.myPlayer = 1;
+			spawntimer1 = 0;
 		}
-		if(!player2IsActive && Input.GetButtonDown("JumpP2"+_isMac)){
+		if(!player2IsActive && Input.GetButtonDown("JumpP2"+_isMac) && spawntimer2 > TimeToWait)
+		{
 			player2IsActive = true;
 			PlayerScript p1 = ((GameObject)Instantiate(player, new Vector3(-3f, -3f, 20), Quaternion.identity)).GetComponent<PlayerScript>();
 			p1.myPlayer = 2;
+			spawntimer2 = 0;
 		}
-		if(!player3IsActive && Input.GetButtonDown("JumpP3"+_isMac)){
+		if(!player3IsActive && Input.GetButtonDown("JumpP3"+_isMac) && spawntimer3 > TimeToWait)
+		{
 			player3IsActive = true;
 			PlayerScript p1 = ((GameObject)Instantiate(player, new Vector3(-0f, -3f, 20), Quaternion.identity)).GetComponent<PlayerScript>();
 			p1.myPlayer = 3;
+			spawntimer3 = 0;
 		}
-		if(!player4IsActive && Input.GetButtonDown("JumpP4"+_isMac)){
+		if(!player4IsActive && Input.GetButtonDown("JumpP4"+_isMac) && spawntimer4 > TimeToWait)
+		{
 			player4IsActive = true;
 			PlayerScript p1 = ((GameObject)Instantiate(player, new Vector3(3f, -3f, 20), Quaternion.identity)).GetComponent<PlayerScript>();
 			p1.myPlayer = 4;
+			spawntimer4 = 0;
 		}
 	
 	
