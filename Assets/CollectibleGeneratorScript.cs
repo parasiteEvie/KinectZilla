5f02ee8422fd49e5545fcf05e1ce6e76945683c3
AbbieDoobie <nathan.p.zager@gmail.com>
2014-09-05 14:24:18 -0700

    Added bomb to collectible manager

--- Assets/CollectibleGeneratorScript.cs
+++ Assets/CollectibleGeneratorScript.cs
@@ -21,21 +21,34 @@ public class CollectibleGeneratorScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(timer > 0f){
+	void Update () 
+	{
+		if(timer > 0f)
+		{
 			timer -= Time.deltaTime;
 		}
-		else{
-			GetRandomGenTime();
-			//generate a collectible
+		else
+		{
+				GetRandomGenTime();
+				//generate a collectible
+			if (Random.Range(0,2) == 0)
+			{
 			GameObject.Instantiate(healthPack, new Vector3((float)Random.Range (-30f, 30f), 
-			                                               (float)Random.Range (-10.5f, -7.5f),
-			                                               (float)Random.Range (15f, 21f)), Quaternion.identity);
+				                                               (float)Random.Range (-10.5f, -7.5f),
+				                                               (float)Random.Range (15f, 21f)), Quaternion.identity);
+			}
+			else
+			{
+				Debug.Log ("Bomb");
+				GameObject.Instantiate(bomb, new Vector3((float)Random.Range (-30f, 30f), 
+				                                               (float)Random.Range (-10.5f, -7.5f),
+				                                               (float)Random.Range (15f, 21f)), Quaternion.identity);
+			}
 		}
 	
 	}
 
 	void GetRandomGenTime () {
-		timer = (float)Random.Range (1, 5);
+		timer = (float)Random.Range (1, 4);
 	}
 }
