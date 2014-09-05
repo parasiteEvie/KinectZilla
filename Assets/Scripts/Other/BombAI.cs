77030a35c4201a3eedcb21a62a7b70f4504cc066
AbbieDoobie <nathan.p.zager@gmail.com>
2014-09-05 16:06:00 -0700

    Bomb logic fixes

---
+++ Assets/Scripts/Other/BombAI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombAI : MonoBehaviour {
+	
+	public float speed;
+	
+	
+	public Vector3 targetDirection;
+	
+	// Update is called once per frame
+	void Update () {
+		if (targetDirection == Vector3.zero){
+			speed = speed/5f;
+			targetDirection = Vector3.up;
+		}
+		Vector3 newPosition = transform.position;
+		newPosition = newPosition + targetDirection * speed * Time.deltaTime;
+		transform.position = newPosition;
+		
+	}
+	
+	void OnBecameInvisible() {
+		Destroy(gameObject);
+	}
+}
